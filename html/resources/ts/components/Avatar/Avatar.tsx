c6ed2c43276cf2688aa8de437e95324eff499b01
k.ishiwata <ki.ishiwata@gmail.com>
2023-11-03 17:11:08 +0900

    Avatarコンポーネント：文字表示対応

--- html/resources/ts/components/Avatar/Avatar.tsx
+++ html/resources/ts/components/Avatar/Avatar.tsx
@@ -1,10 +1,34 @@
 import { styled } from '@/stitches.config'
+import React, { ReactNode } from 'react'
 
-export const Avatar = styled('div', {
+const Wrap = styled('div', {
     width: 36,
     height: 36,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     overflow: 'hidden',
     borderRadius: '50%',
+    background: '$gray',
+    color: '$white',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    userSelect: 'none',
+    cursor: 'pointer',
 })
+
+type Props = {
+    src?: string
+    children?: ReactNode
+}
+
+export const Avatar: React.FC<Props> = ({
+    src,
+    children
+}) => {
+    return (
+        <Wrap css={{backgroundImage: `url(${ src })`}}>
+            { src ? '' : children }
+        </Wrap>
+    )
+}
